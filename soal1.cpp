eded5e9541147b0bc6b916edafe2f44d7f56fbe5
cyrillans <cyrillanathania@students.usu.ac.id>
2025-04-19 18:10:40 +0700

    JAWABAN_UTS_AP1

---
+++ soal1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Kode: ";
+    cin >> n;
+
+    if (n <= 0) 
+    {
+        cout << "bukan" << endl;
+    }
+
+    while (n % 2 == 0) 
+    {
+        n/=2;
+    }
+    
+    if (n == 1)
+    {
+        cout << "ya" << endl;
+    }
+    else
+    { 
+        cout << "bukan" << endl;
+    }
+}
